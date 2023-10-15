a846df94592977cee2ce65df16678629e11fba0c
Stephon <stephonhorton222@gmail.com>
2023-10-15 19:39:40 -0400

    updated server

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.post('/email', (req, res) => {
   let day = req.body.day;
   let year = req.body.year;
   let time = req.body.time;
+  let style = req.body.style;
+  let price = req.body.price;
 
   // Process the data or send a response
   console.log(`Received data: Name - ${name}, Email - ${email}, Phone - ${phone}`);
@@ -41,37 +43,38 @@ app.post('/email', (req, res) => {
     from: 'beautyboss48235@gmail.com',
     to: `${email}`,
     subject: 'Appointment with the Beauty Boss',
-    text: `Thank you for booking with the Beauty Boss. Your appointment is on ${month} ${day}, ${year} at ${time} for any questions contact your stylist at 313-444-4444`,
+    text: `Thank you for booking with The Beauty Boss. Your appointment for a ${style} ${price} is on ${month} ${day}, ${year} at ${time}. Your stylist will contact you soon with details, for any questions contact your stylist at 313-444-4444`,
   };
 
 
-  const mailOptions2 = {
-    from: 'beautyboss48235@gmail.com',
-    to: 'hortonstephon12@gmail.com',
-    subject: 'New Appointment Booked',
-    text: `you have an appointment with ${name} on ${month} ${day}, ${year} at ${time} call ${phone} or email ${email} to contact client`,
-  };
 
   transporter.sendMail(mailOptions1, (error, info) => {
     if (error) {
       console.error('Error sending email to client: ', error);
-      res.json({ message: 'Error sending email' });
+      res.json({ message: 'Error booking appointment, please correct email' });
      
     } else {
       console.log('Email sent to client successfully: ', info.response);
-      res.json({ message: 'Email sent successfully' });
-    }
-  });
+      res.json({ message: 'Booking Successful! Check email for appointment details' });
 
-  transporter.sendMail(mailOptions2, (error, info) => {
-    if (error) {
-      console.error('Error sending email to stylist: ', error);
-    } else {
-      console.log('Email sent to stylist successfully: ', info.response);
+      const mailOptions2 = {
+        from: 'beautyboss48235@gmail.com',
+        to: 'hortonstephon12@gmail.com',
+        subject: 'New Appointment Booked',
+        text: ` You have a booking request for a ${style} ${price} with ${name} on ${month} ${day}, ${year} at ${time} call ${phone} or email ${email} to contact client`,
+      };
+
+      transporter.sendMail(mailOptions2, (error, info) => {
+        if (error) {
+          console.error('Error sending email to stylist: ', error);
+        } else {
+          console.log('Email sent to stylist successfully: ', info.response);
+        }
+      });
+
     }
   });
 });
-
 
 
 app.listen(process.env.PORT || PORT, () => {
